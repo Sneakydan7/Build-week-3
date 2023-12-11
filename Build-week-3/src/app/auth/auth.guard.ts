d0b77be30109c61c26e9ab4ffef8fb5bcafcfbf9
Daniele Cagnoni <145670818+Sneakydan7@users.noreply.github.com>
2023-12-11 16:57:08 +0100

    wip

--- Build-week-3/src/app/auth/auth.guard.ts
+++ Build-week-3/src/app/auth/auth.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree , Router } from '@angular/router';
+import { Observable , map, take } from 'rxjs';
+import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+
+constructor(private authSrv: AuthService, private router: Router){}
+
+
+canActivate(
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+):
+  | Observable<boolean | UrlTree>
+  | Promise<boolean | UrlTree>
+  | boolean
+  | UrlTree {
+  return this.authSrv.user$.pipe(
+    take(1),
+    map((user) => {
+      if (user) {
+        return true;
+      }
+      alert(
+        'Per visualizzare questa risorsa devi essere loggato!\nAccedi o registrati'
+      );
+      return this.router.createUrlTree(['/login']);
+    })
+  );
+}
 }
--- Build-week-3/src/app/auth/register/register.component.ts
+++ Build-week-3/src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import {  FormGroup, NgForm, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+
+
+  constructor(private authSrv: AuthService , private router: Router ) {
+   }
 
   ngOnInit(): void {
   }
+
+register(form:NgForm){
+  try{
+    this.authSrv.register(form.value).subscribe()
+    console.log(form.value)
+  } catch(error:any){
+    alert(error)
+    this.router.navigate(['/register'])
+  }
+
+}
+
+// validateForm(data: {name:string , lastname:string , email:string , password:string }){
+//   this.registrationForm = this.fb.group({
+//      name: [
+//       '' , [Validators.required , Validators.minLength(2) , Validators.pattern(/^[a-zA-Z]+$/),]
+//     ],
+//      lastname:[
+//       '' , [Validators.required , Validators.minLength(2) , Validators.pattern(/^[a-zA-Z]+$/),]
+//      ],
+//      email:[
+//       '' , [Validators.required , Validators.email]
+//      ],
+//      password:[
+//       '' , [Validators.required , Validators.minLength(8) ]
+//      ],
+
+//   })
+
+// }
 
 }
