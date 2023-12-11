546dc27f4286a16d43a0f11d8eb536e4006d4b8e
Daniele Cagnoni <145670818+Sneakydan7@users.noreply.github.com>
2023-12-11 12:34:22 +0100

    added components and authentication

--- Build-week-3/src/app/app.module.ts
+++ Build-week-3/src/app/app.module.ts
@@ -1,14 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
+import { FormsModule  } from '@angular/forms';
+import { TokenInterceptor } from './auth/token.interceptor';
+import { HTTP_INTERCEPTORS , HttpClientModule } from '@angular/common/http';
+import { AuthGuard } from './auth/auth.guard';
+
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewComponent } from './components/view/view.component';
+import { EditComponent } from './components/edit/edit.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+const routes :Route[] = [
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  {
+    path:'view',
+    component:ViewComponent
+  },
+  {
+    path:'edit',
+    component:EditComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ViewComponent,
+    EditComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , RouterModule.forRoot(routes) , FormsModule  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
