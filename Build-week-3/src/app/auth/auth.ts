69ce51e56e2ba7ee67c127fafa3c376edef177f4
GianniCbd <giannicabiddu@gmail.com>
2023-12-12 13:28:34 +0100

    register update

--- Build-week-3/src/app/auth/auth.ts
+++ Build-week-3/src/app/auth/auth.ts
@@ -1,9 +1,10 @@
 export interface Auth {
-    accessToken:string
-    user:{
-        userId:number
-        email:string
-        name:string
-        lastName:string
-    }
+  accessToken: string;
+  user: {
+    userId: number;
+    email: string;
+    name: string;
+    lastName: string;
+    biografia: string;
+  };
 }
